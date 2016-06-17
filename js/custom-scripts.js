ba2f2a8098438d67e37aa1666c98f836884b8d3d
Iegor Stetsiura <biropka@gmail.com>
2016-06-17 17:05:23 +0300

    v 0.1.2

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -12,4 +12,29 @@ $(document).ready(function () {
         $('nav ul').slideToggle();
     });
 
+    $('input[type="submit"]').click(function() {
+        $(this).parent().find('.form-field').css('border', '1px solid #ff8a00');
+    });
+
+    jQuery(function($) {
+        $('form').validatr({
+            showall: true
+        });
+    });
+
+    jQuery(function($){
+        $('.user-phone').mask("+7(999) 999-99-99");
+    });
+
+    /*** window-callback ***/
+    $('.leave-order').click(function () {
+        $('.window-callback-short').fadeIn();
+    });
+
+    $('.window-callback-short').click(function (event) {
+        $target = $(event.target);
+        if (!$target.closest($('.form-callback-short')).length) $('.window-callback-short').fadeOut();
+    });
+    /*** the end of the window-callback script ***/
+
 });
