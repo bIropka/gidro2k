1c7a76589b0cdd15850967df2d5f280e6bddb650
Iegor Stetsiura <biropka@gmail.com>
2016-06-18 00:45:04 +0300

    v 0.1.4

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -17,8 +17,56 @@ $(document).ready(function () {
         $(this).parent().find('.form-field:valid').css('border', '1px solid transparent');
     });
 
-    jQuery(function($) {
-        $('form').validatr({
+    /** validation **/
+
+    $(function($) {
+        $('#form-top').validatr({
+            showall: true,
+            valid: submitFormTop
+        });
+    });
+    function submitFormTop() {
+        $('.window-callback-full').fadeIn();
+        $('#full-user-name').val($('#top-user-name').val());
+        $('#full-user-email').val($('#top-user-email').val());
+        $('#full-user-phone').val($('#top-user-phone').val());
+        $('#form-callback-full').find('.form-field:valid').css('border', '1px solid transparent');
+        return false;
+    }
+
+    $(function($) {
+        $('#form-bottom').validatr({
+            showall: true,
+            valid: submitFormBottom
+        });
+    });
+    function submitFormBottom() {
+        $('.window-callback-full').fadeIn();
+        $('#full-user-name').val($('#bottom-user-name').val());
+        $('#full-user-email').val($('#bottom-user-email').val());
+        $('#full-user-phone').val($('#bottom-user-phone').val());
+        $('#form-callback-full').find('.form-field:valid').css('border', '1px solid transparent');
+        return false;
+    }
+
+    $(function($) {
+        $('#form-callback-short').validatr({
+            showall: true,
+            valid: submitFormShort
+        });
+    });
+    function submitFormShort() {
+        $('#form-callback-short').fadeOut();
+        $('.window-callback-full').fadeIn();
+        $('#full-user-name').val($('#short-user-name').val());
+        $('#full-user-email').val($('#short-user-email').val());
+        $('#full-user-phone').val($('#short-user-phone').val());
+        $('#form-callback-full').find('.form-field:valid').css('border', '1px solid transparent');
+        return false;
+    }
+
+    $(function($) {
+        $('#form-callback-full').validatr({
             showall: true
         });
     });
@@ -27,11 +75,12 @@ $(document).ready(function () {
         $('.user-phone').mask("+7(999) 999-99-99");
     });
 
+    /** end of validation **/
+
     /*** window-callback-short ***/
     $('.leave-order').click(function () {
         $('.window-callback-short').fadeIn();
     });
-
     $('.window-callback-short').click(function (event) {
         $target = $(event.target);
         if (!$target.closest($('.form-callback-short')).length) $('.window-callback-short').fadeOut();
@@ -39,6 +88,9 @@ $(document).ready(function () {
     /*** the end of the window-callback-short script ***/
 
     /*** window-callback-full ***/
+    $('.get-callback').click(function () {
+        $('.window-callback-full').fadeIn();
+    });
     $('.window-callback-full').click(function (event) {
         $target = $(event.target);
         if (!$target.closest($('.form-callback-full')).length) $('.window-callback-full').fadeOut();
