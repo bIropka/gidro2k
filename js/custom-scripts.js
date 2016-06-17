0daf858a20621449cf915cd37873a981e8da1d18
Iegor Stetsiura <biropka@gmail.com>
2016-06-17 03:08:41 +0300

    v 0.1.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
 
+    function windowSize(){
+        if ($(window).width() < '762'){
+            var formTop = $('.service-info-page .form-top');
+            formTop.insertBefore($('.services-cost'));
+        }
+    }
+    jQuery(window).load(windowSize);
 
+    $('.mobile-menu').click(function() {
+        $('nav ul').slideToggle();
+    });
 
 });
